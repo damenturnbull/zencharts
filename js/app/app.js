2798a33a1d600df2a70483adc9bae9284b546a1e
Damen Turnbull <info@fando.com.au>
2015-04-23 23:42:26 +1000

    Add basic stubbing methods for Ajax request using OAuth.

--- js/app/app.js
+++ js/app/app.js
@@ -1,5 +1,5 @@
 // My Application
-var MyApp = (function($) {
+var GraphApp = (function($) {
   'use strict';
 
   return {
@@ -10,31 +10,67 @@ var MyApp = (function($) {
     },
 
     init: function() {
-      this.requestJSON();
-        $('#graphA').highcharts(graphA);
-        $('#graphB').highcharts(graphB);
-        $('#graphC').highcharts(graphC);
+      $('#graphA').highcharts(graphA);
+      $('#graphB').highcharts(graphB);
+      $('#graphC').highcharts(graphC);
+      this.checkForURLHash();
     },
 
-    requestJSON: function() {
-      // Request JSON
-      var jqxhr = $.getJSON("/js/app/mock_data/tickets.json")
-        .done  ( this.handleDone )
-        .fail  ( this.handleFail )
-        .always( this.handleAlways );
+    // Check for the existence of '#error=' or '#access_token' in the URL
+    checkForURLHash: function() {
+      console.log("Checking for URL Hash...");
+      var error = this.getHashParameter("error");
+      var token = this.getHashParameter("access_token");
+
+      if(error) {
+        console.log('Error exists in URL.');
+      } else if(token) {
+        console.log('Token exists in URL.');
+        this.requestJSON(token);
+      } else {
+        console.log('No error or token in URL.');
+      }
     },
 
-    handleDone: function(data) {
-      console.log( "success" );
-      // console.log( data );
+    // Decode URL and search for string
+    // http://stackoverflow.com/questions/1403888/get-url-parameter-with-jquery
+    getHashParameter: function(name) {
+      return decodeURIComponent((new RegExp('[#|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.hash)||[,""])[1].replace(/\+/g, '%20'))||null;
+    }, 
+
+    requestJSON: function(token) {
+      var ajax_options = {
+        method: 'GET',
+        // Add a Basic Auth token in the header
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader("Authorization", "Bearer " + token);
+        }
+      };
+      // Make the request...
+      $.ajax('https://fando.zendesk.com/api/v2/tickets.json', ajax_options)
+        .done     ( this.handleDone )
+        .fail     ( this.handleFail )
+        .complete ( this.handleComplete )
+        .always   ( this.handleAlways );
     },
 
-    handleFail: function(data) {
-      console.log( "fail" );
-      // console.log( data );
+    handleDone: function(data, textStatus, xhr) {
+      console.log( "Ajax success." );
+      console.log( data );
     },
 
-    handleAlways: function(data) {
+    handleFail: function(xhr, textStatus, errorThrown) {
+      console.log( "Ajax fail." );
+      console.log(xhr);
+      console.log(textStatus);
+      console.log(errorThrown);
+    },
+
+    handleComplete: function() {
+      console.log("Ajax complete, hide spinner.");      
+    },
+
+    handleAlways: function() {
       console.log( "always" );
       // console.log( data );
     }
@@ -44,4 +80,4 @@ var MyApp = (function($) {
 }(jQuery));
 
 // Runner
-$(document).ready(MyApp.init());
+$(document).ready(GraphApp.init());
