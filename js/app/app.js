49f3a1b99ef9d04c5596036890957f57eb2cf742
Damen Turnbull <info@fando.com.au>
2015-04-24 16:33:26 +1000

    Refactor button style logic.

--- js/app/app.js
+++ js/app/app.js
@@ -135,7 +135,55 @@ var GraphA = (function($) {
   };
 }(jQuery));
 
+var GraphB = (function($) {
 
+  return { 
+
+    init: function(data) {
+      var percentages = this.getPercentages(data);
+      graphB.series[0].data[0].y = percentages[0];
+      graphB.series[0].data[1].y = percentages[1];    
+      $('#graphB').highcharts(graphB);           
+    }, 
+
+    setCutOffDate: function(days_before) {
+      var date = new Date(); // Today
+      return date.setDate( date.getDate() - days_before );   
+    },
+
+    // TODO tighten date adjustment to account for GMT
+    // Currently the time is ignored
+    convertZendeskDate: function(raw_date) {
+      var date_bits = raw_date.split("T");
+      var date      = date_bits[0];
+      var time      = date_bits[1];
+      return new Date(date);
+    },
+
+    getPercentages: function(json_data) {
+      var end_users       = json_data.users;
+      var count_logged_in = 0;
+
+      for(var i = 0; i < json_data.count; i++) {
+        var last_login_at = end_users[i].last_login_at;
+        // Don't count users that have never logged in
+        if(last_login_at == null) break;
+        // Get dates to compare
+        var last_login_at   = this.convertZendeskDate(last_login_at);
+        var cutoff_date     = this.setCutOffDate(15);
+        // Count...
+        if(last_login_at >= cutoff_date) {
+          count_logged_in++;
+        }
+      }
+      // Calculate percentages
+      var perc_logged_in      = (Math.round((count_logged_in / json_data.count) * 100));
+      var perc_not_logged_in  = 100 - perc_logged_in;
+      return [perc_logged_in, perc_not_logged_in];
+    }
+
+  };
+}(jQuery));
 
 // ------------------------------
 // ------------------------------
@@ -151,6 +199,21 @@ $(document).ready(function(){
     // Reference: http://stackoverflow.com/questions/1403888/get-url-parameter-with-jquery
     return decodeURIComponent((new RegExp('[#|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.hash)||[,""])[1].replace(/\+/g, '%20'))||null;
   }
+
+  // Set style of mock and live buttons
+  function setButtons(liveData) {
+    var btn_mock        = $('#btnMockReports');
+    var btn_live        = $('#btnLiveReports');
+    var class_selected  = 'btn--selected';
+    if(liveData) {
+      btn_mock.removeClass(class_selected);
+      btn_live.addClass(class_selected);
+    } else {
+      btn_mock.addClass(class_selected);
+      btn_live.removeClass(class_selected);
+    }
+  }
+
   var url_access_token = getURLHashParameter("access_token");
   var url_error        = getURLHashParameter("error");
 
@@ -163,9 +226,9 @@ $(document).ready(function(){
     console.log('access_token in the URL.');
     // Set URLs for live data
     graph_a_datasource = 'https://fando.zendesk.com/api/v2/users/search.json?role=end-user';
+    
     // Setup buttons
-    $('#btnMockReports').removeClass('btn--selected');
-    $('#btnLiveReports').addClass('btn--selected');
+    setButtons(true);
 
   } else {
     console.log('No access_token in URL.');
@@ -174,10 +237,8 @@ $(document).ready(function(){
       console.log('Error exists in URL.');
     }
 
+    setButtons(false);
     graph_a_datasource = '/js/app/mock_data/end_users.json';
-    GraphA.init('/js/app/mock_data/end_users.json');
-    $('#btnMockReports').addClass('btn--selected');
-    $('#btnLiveReports').removeClass('btn--selected');
   }  
 
   // Render graphs (Mock or Live)
